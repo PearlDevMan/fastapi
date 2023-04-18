4cfdb7130210da83b51e472407d57907563a8429
Alexander.L <arakihonda526@gmail.com>
2023-04-18 03:25:52 -0400

    cert

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ async def submit(nm:str = Form(...), pwd:str = Form(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host = '0.0.0.0', port = 8000, reload = True)
+    uvicorn.run('main:app', host='0.0.0.0', port=443, ssl_keyfile='./self_cert.key', ssl_certfile='./min_certfile.cst')
